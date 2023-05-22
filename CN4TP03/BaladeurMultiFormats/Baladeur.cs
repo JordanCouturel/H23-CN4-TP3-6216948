f2c2b8133fdf232987a1ad76d6ab4cb689f445c7
Jordan Couture-Lafranchise <6216948@cegepmontpetit.ca>
2023-05-22 15:53:10 -0400

    Compléter le code de la méthode ConstruireLaListeDesChansons

--- CN4TP03/BaladeurMultiFormats/Baladeur.cs
+++ CN4TP03/BaladeurMultiFormats/Baladeur.cs
@@ -1,12 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace BaladeurMultiFormats
 {
-    internal class Baladeur
+    public class Baladeur : IBaladeur
     {
+        private const string NOM_RÉPERTOIRE = "Chansons";
+        private List<Chanson> m_colChansons;
+
+        public int NbChansons
+        {
+            get { return m_colChansons.Count; }
+        }
+
+        public Baladeur()
+        {
+            m_colChansons = new List<Chanson>();
+        }
+
+        public Chanson ChansonAt(int pIndex)
+        {
+            return m_colChansons[pIndex];
+        }
+
+        public void ConstruireLaListeDesChansons()
+        {
+            if (Directory.Exists(NOM_RÉPERTOIRE))
+            {
+                string[] songslist = Directory.GetFiles(NOM_RÉPERTOIRE);
+                Array.Sort(songslist);
+                foreach (string chansons in songslist)
+                {
+
+                    try
+                    {
+                        string[] titreEtformat = chansons.Split('.');
+                        Chanson chanson;
+                        switch (titreEtformat[1])
+                        {
+                            case "aac":
+                                chanson = new ChansonAAC(chansons);
+                                m_colChansons.Add(chanson);
+                                break;
+                            case "mp3":
+                                chanson = new ChansonMP3(chansons);
+                                m_colChansons.Add(chanson);
+                                break;
+                            case "wma":
+                                chanson = new ChansonWMA(chansons);
+                                m_colChansons.Add(chanson);
+                                break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+            }
+        }
+        public void AfficherLesChansons(ListView pListView)
+        {
+        
+
+        }
+
+        public void ConvertirVersAAC(int pIndex)
+        {
+          
+        }
+        public void ConvertirVersMP3(int pIndex)
+        {
+
+
+         
+        }
+
+        public void ConvertirVersWMA(int pIndex)
+        {
+           
+        }
     }
 }
