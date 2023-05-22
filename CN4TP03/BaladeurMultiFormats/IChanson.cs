f9789595fb12761afba1ac0cb6a05049154cb99e
Jordan Couture-Lafranchise <6216948@cegepmontpetit.ca>
2023-05-22 15:43:37 -0400

    Définir l'interface IChanson

---
+++ CN4TP03/BaladeurMultiFormats/IChanson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BaladeurMultiFormats
+{
+    public interface IChanson
+    {
+        //proprietes
+        int Annee { get; }
+
+        string Artiste { get; }
+
+        string Format { get; }
+
+        string NomFicher { get; }
+
+        string Paroles { get; }
+
+        string Titre { get; }
+
+        //methodes
+
+        void Ecrire(string pParoles);
+
+        void EcrireEntete(StreamWriter pobjFichier);
+
+        void EcrireParoles(StreamWriter pobjFichier, string pParoles);
+
+        void LireEntete();
+
+        string LireParoles(StreamReader pobjFichier);
+
+        void SauterEntete(StreamReader pobjFichier);
+
+    }
+}
+
