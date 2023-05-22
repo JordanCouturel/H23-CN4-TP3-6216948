b02cca7ed83b5557ee30e61c0070a771530f40fa
Jordan Couture-Lafranchise <6216948@cegepmontpetit.ca>
2023-05-22 15:56:51 -0400

    Définir les 4 tests pour la classe Consultation

--- CN4TP03/BaladeurMultiFormatsTests/UnitTestConsultationTODOs.cs
+++ CN4TP03/BaladeurMultiFormatsTests/UnitTestConsultationTODOs.cs
@@ -30,70 +30,87 @@ namespace BaladeurMultiFormats.Tests
         // TODO Test A : ConsultationTestPourUneConsulationParam2NullTest
         // Compléter la méthode pour tester le constructeur au cas où la chanson passée en 
         // deuxième paramètre est à null
-
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void ConsultationTestPourUneConsulationParam2NullTest()
         {
 
             // Arrange/Act : Instancier un objet Consultation avec la date actuelle et une chanson à null
             // À compléter...
+            Consultation m_objConsultation = new Consultation(DateTime.Now, null);
 
-           
+
             // Assert : Vérifier si le constructeur lève une exception ArgumentNullException
             // À compléter...
+
+
+
+
         }
 
         // TODO Test B : ConsultationTestParamètreDateTest
         // Compléter la méthode pour tester si la propriété Date de la classe Consultation
-        
-       
+
+        [TestMethod()]
         public void ConsultationTestParamètreDateTest()
         {
             // Arrange : Instancier un objet ChansonsAAC 
             // Instancier un objet DateTime pour la date actuelle
             // Instancier un objet consultation en utilisant les deux objets que vous venez de créer
             // À compléter...
+            ChansonAAC chanson = new ChansonAAC("Chansons\\Imagine.aac");
+            DateTime date = DateTime.Now;
+            Consultation consultation = new Consultation(date, chanson);
+
 
             // Act : Récupérer la date de consultation de la chanson en utilisant la propriété Date 
             // À compléter...
-
+            DateTime dateconsultation = consultation.Date;
             // Assert : Vérifier si la propriété Date retourne la bonne date
             // À compléter...
-
+            Assert.AreEqual(date, dateconsultation);
 
         }
 
         // TODO Test C : ConsultationTestParamètreDélaiTest
         // Compléter la méthode pour tester la propriété Délai
+        [TestMethod()]
         public void ConsultationTestParamètreDélaiTest()
         {
             // Arrange : Instancier un objet ChansonsAAC 
             // Instancier un objet DateTime pour le 1er janvier 2021
             // Instancier un objet consultation en utilisant les deux objets que vous venez de créer
             // À compléter...
-
+            ChansonAAC chanson = new ChansonAAC("Chansons\\Imagine.aac");
+            DateTime date = new DateTime(2021, 1, 1);
+            Consultation consultation = new Consultation(date, chanson);
             // Act : Récupérer le délai de la en utilisant la propriété Délai
             // À compléter...
-
+            int delai = consultation.Délai;
 
             // Assert : Vérifier si la propriété Délai calcule et retourne le bon délai 
             // À compléter...
-
+            int delaiAttendu = (int)((DateTime.Now - date).TotalSeconds);
+            Assert.AreEqual(delaiAttendu, delai);
         }
         // TODO Test D : ConsultationTestParamètreChansonTest
         // Compléter la méthode pour tester la propriété LaChanson
+        [TestMethod()]
         public void ConsultationTestParamètreChansonTest()
         {
             // Arrange : Instancier un objet ChansonsAAC 
             // Instancier un objet consultation avec la date actuelle et l'objet ChansonAAC
             // À compléter...
-
+            ChansonAAC chanson = new ChansonAAC("Chansons\\Imagine.aac");
+            DateTime date = DateTime.Now;
+            Consultation consultation = new Consultation(date, chanson);
             // Act : Récupérer la chanson avec la propriété LaChanson
             // À compléter...
-
+            Chanson chanson1 = consultation.LaChanson;
 
             // Assert : Vérifier si la propriété LaChanson retourne la bonne chanson
             // À compléter...
-
+            Assert.AreEqual(chanson1, chanson);
         }
     }
 }
